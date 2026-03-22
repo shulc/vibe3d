988f006718985b92429d06a5ce977b9d29e4e0ea
Alexander Shagarov <alexander.shagarov@gmail.com>
2026-03-22 16:53:17 +0300

    change shift-D to subdivide

--- source/app.d
+++ source/app.d
@@ -352,14 +352,6 @@ void main(string[] args) {
                         case SDLK_1:      editMode = EditMode.Vertices;  break;
                         case SDLK_2:      editMode = EditMode.Edges;     break;
                         case SDLK_3:      editMode = EditMode.Polygons;  break;
-                        case SDLK_TAB:
-                            setActiveTool(null);
-                            mesh = catmullClark(mesh);
-                            selected.length      = mesh.vertices.length; selected[]      = false;
-                            selectedEdges.length = mesh.edges.length;    selectedEdges[] = false;
-                            selectedFaces.length = mesh.faces.length;    selectedFaces[] = false;
-                            gpu.upload(mesh);
-                            break;
                         case SDLK_SPACE:
                             if (activeTool) setActiveTool(null);
                             else editMode = cast(EditMode)((cast(int)editMode + 1) % 3);
@@ -370,6 +362,19 @@ void main(string[] args) {
                         case SDLK_r:
                             setActiveTool(cast(ScaleTool)activeTool ? null : getScaleTool());
                             break;
+                        case SDLK_d: {
+                                bool shift = (event.key.keysym.mod & KMOD_SHIFT) != 0;
+                                if (shift) {
+                                    setActiveTool(null);
+                                    mesh = catmullClark(mesh);
+                                    selected.length      = mesh.vertices.length; selected[]      = false;
+                                    selectedEdges.length = mesh.edges.length;    selectedEdges[] = false;
+                                    selectedFaces.length = mesh.faces.length;    selectedFaces[] = false;
+                                    gpu.upload(mesh);
+                                }
+                            }
+                            break;
+
                         default: break;
                     }
                     break;
